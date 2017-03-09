759c29265fef025e970981e2a905f1d95f97f84a
Marco Trevisan (Treviño) <mail@3v1n0.net>
2017-03-09 17:01:25 +0100

    preload: use PATH_MAX as limit with null ending char

--- preload.c
+++ preload.c
@@ -496,13 +496,14 @@ socket_action (int (*action) (int sockfd, const struct sockaddr *addr, socklen_t
     int result = 0;
     char *new_path = redirect_path (un_addr->sun_path);
 
-    if (strncmp (un_addr->sun_path, new_path, sizeof (un_addr->sun_path)) == 0) {
+    if (strncmp (un_addr->sun_path, new_path, PATH_MAX) == 0) {
         result = action (sockfd, addr, addrlen);
     } else {
         struct sockaddr_un new_addr = {0};
         new_addr.sun_family = AF_UNIX;
-        strncpy (new_addr.sun_path, new_path, sizeof (new_addr.sun_path));
-        result = action (sockfd, (const struct sockaddr *)&new_addr, sizeof(new_addr));
+        new_addr.sun_path[0] = '\0';
+        strncat (new_addr.sun_path, new_path, PATH_MAX - 1);
+        result = action (sockfd, (const struct sockaddr *) &new_addr, sizeof (new_addr));
     }
 
     free (new_path);
